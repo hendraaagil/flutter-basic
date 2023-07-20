f0a2b19e6130c6c74cc48cc9918549d3bf3fc8d2
hendraaagil <hendraaagil@gmail.com>
2023-07-20 15:21:40 +0700

    feat: adding interactivity tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,56 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
+class FavoriteWidget extends StatefulWidget {
+  const FavoriteWidget({super.key});
+
+  @override
+  State<FavoriteWidget> createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            padding: const EdgeInsets.all(0),
+            alignment: Alignment.centerRight,
+            icon: (_isFavorited
+                ? const Icon(Icons.star)
+                : const Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+            width: 18,
+            child: SizedBox(
+              child: Text('$_favoriteCount'),
+            ))
+      ],
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -28,8 +78,7 @@ class MyApp extends StatelessWidget {
               )
             ],
           )),
-          Icon(Icons.star, color: Colors.red[500]),
-          const Text('41')
+          const FavoriteWidget(),
         ],
       ),
     );
